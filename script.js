539718dbbc51e86e341eca28c5783ff3ffb0ad44
tmannazy <e.anene@hotmail.com>
2021-11-05 22:08:28 +0100

    Edit logic to display image on circle clicks

--- script.js
+++ script.js
@@ -13,33 +13,40 @@ const displaySlides = (slideNum) => {
   [...getSlides].forEach((item, index) => {
     item.style.display = "none";
   });
-  //   if (slideIndex === 1 && slideIndex < 3) {
-  //     nextBtn.addEventListener("mousemove", () => {
-  //       nextBtn.classList.toggle("change-colors");
-  //     });
-  //     prevBtn.addEventListener("mousemove", () => {
-  //       prevBtn.classList.toggle("change-colors");
-  //     });
-  //   }
+  [...getDots].forEach((item) => {
+    item.classList.remove("active");
+  });
 
   getSlides[slideIndex - 1].setAttribute("style", "display: block");
+  getDots[slideIndex - 1].classList.add("active");
 };
 
 const slideShow = (num) => {
   displaySlides((slideIndex += num));
 };
 
-function changeDisplay() {
+const changeDisplay = () => {
   slideShow(1);
   runSlides = setTimeout(changeDisplay, 5000);
-}
+};
+
+const circleDisplay = (circleNum) => {
+  slideShow(circleNum);
+};
 
 nextBtn.addEventListener("click", () => {
   slideShow(1);
 });
+
 prevBtn.addEventListener("click", () => {
   slideShow(-1);
 });
+
+[...dotBtn].forEach((item, index) =>
+  item.addEventListener("click", () => {
+    circleDisplay(index);
+  })
+);
 
 displaySlides(slideIndex);
 let runSlides = setTimeout(changeDisplay, 5000);
